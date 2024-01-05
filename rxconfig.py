00f522a41992f1bd78f31ae4f8cce6aaa2aedd99
lopezrbn <lopezrbn@gmail.com>
2024-01-05 02:49:11 +0100

    Updated cars scraped.

--- rxconfig.py
+++ rxconfig.py
@@ -3,7 +3,7 @@ import socket
 
 config = rx.Config(
     app_name="car_prices_checker",
-    api_url=("http://152.228.134.180" if socket.gethostname() == "vps-955fe093" else "http://localhost:8001"),
-    frontend_port=3001,
-    backend_port=8001,
+    api_url=("http://152.228.134.180:8000" if socket.gethostname() == "vps-955fe093" else "http://localhost:8000"),
+    frontend_port=3000,
+    backend_port=8000,
 )
