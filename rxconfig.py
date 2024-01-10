981d2fd4ccb932aa5c54a84fcfbecdecc9bd40c3
lopezrbn <lopezrbn@gmail.com>
2024-01-10 19:58:12 +0100

    Configuration of rxconfig.py file.

--- rxconfig.py
+++ rxconfig.py
@@ -1,9 +1,12 @@
 import reflex as rx
 import socket
 
+front_port = 3002
+back_port = 8002
+
 config = rx.Config(
     app_name="car_prices_checker",
-    api_url=("http://152.228.134.180:8000" if socket.gethostname() == "vps-955fe093" else "http://localhost:8000"),
-    frontend_port=3000,
-    backend_port=8000,
+    api_url=("http://152.228.134.180" if socket.gethostname() == "vps-955fe093" else f"http://localhost:{back_port}"),
+    frontend_port=front_port,
+    backend_port=back_port,
 )
