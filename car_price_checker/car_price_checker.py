f341c7a6d69cc58551000f15a3aedaf1c339488e
lopezrbn <lopezrbn@gmail.com>
2024-01-16 23:29:34 +0100

    Added tab title.

--- car_price_checker/car_price_checker.py
+++ car_price_checker/car_price_checker.py
@@ -3,6 +3,8 @@ from .pages.index import index
 from .pages.results import results
 
 
+tab_title = "Car price checker"
+
 app = rx.App()
-app.add_page(index)
-app.add_page(results)
+app.add_page(index, title=tab_title)
+app.add_page(results, title=tab_title)
