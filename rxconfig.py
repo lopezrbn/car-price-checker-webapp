13ae046cfb762851a96f5118c5495b8840728b7b
Rubén López <lopezrbn@gmail.com>
2025-02-04 21:51:29 +0100

    Remove api_url to comply with newest version of Reflex

--- rxconfig.py
+++ rxconfig.py
@@ -6,7 +6,8 @@ back_port = 8002
 
 config = rx.Config(
     app_name="car_price_checker",
-    api_url=("http://lopezrbn.sytes.net" if socket.gethostname() == "ubuntu" else f"http://localhost:{back_port}"),
+    # api_url=("http://lopezrbn.sytes.net" if socket.gethostname() == "rubenlocalserver" else f"http://localhost:{back_port}"),
+    # api_url=("http://car-price-checker.lopezrbn.com" if socket.gethostname() == "rubenlocalserver" else f"http://localhost:{back_port}"),
     frontend_port=front_port,
     backend_port=back_port,
 )
